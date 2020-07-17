4bc60d75d6ea847ae0ea7b8ca77aee5c0e125eb7
Etiendo <32451923+Etiendo@users.noreply.github.com>
2020-07-17 10:09:32 +0200

    Fix url bug to change language

--- etna/core/urls.py
+++ etna/core/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:language>/', views.index, name='index'),
+    path('language/<str:language>/', views.index, name='index'),
 ]
